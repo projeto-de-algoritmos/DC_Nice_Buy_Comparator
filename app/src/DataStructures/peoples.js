4656b118cd76eff7f4320d238f903dcbadc96e69
Antonio Ruan Moura Barreto <ruanmoura13@outlook.com>
2021-04-12 23:38:22 -0300

    Add array with person lists

---
+++ app/src/DataStructures/peoples.js
@@ -0,0 +1,23 @@
+import globalRank from './globalRank'
+
+function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+}
+
+const rank = Object.keys(globalRank);
+const AntonioRuan = shuffle(JSON.parse(JSON.stringify(rank)));
+const Wagner = shuffle(JSON.parse(JSON.stringify(rank)));
+const Ian = shuffle(JSON.parse(JSON.stringify(rank)));
+const Rafael = shuffle(JSON.parse(JSON.stringify(rank)));
+const Maria = shuffle(JSON.parse(JSON.stringify(rank)));
+const Joao = shuffle(JSON.parse(JSON.stringify(rank)));
+
+export default [AntonioRuan, Wagner, Ian, Rafael, Maria, Joao]
